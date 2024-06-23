d384ac3dc2679f7c74fe52fc36ad328a1ed3bf83
AdamA-A <adammnashour@gmail.com>
2024-06-23 11:47:21 -0400

    Completely replaced previous library search methods, with the LibrarySearch class

--- test/migration/Search.js
+++ test/migration/Search.js
@@ -3,13 +3,16 @@ class LibrarySearch {
     resultContainerSelector;
     query;
     libraryResults;
+    btnOnclick;
     constructor(useLibby) {
         if (useLibby) {
             this.overdriveInstance = OverDrive.getLibbyInstance();
             this.resultContainerSelector = "#libbyLibrarySearch .searchResults";
+            this.btnOnclick = "Search.libbyLibraries.toggleLibrary(this);";
         } else {
             this.overdriveInstance = OverDrive.getSoraInstance();
             this.resultContainerSelector = "#soraLibrarySearch .searchResults";
+            this.btnOnclick = "Search.soraLibraries.toggleLibrary(this);";
         }
         this.libraryResults = {};
     }
@@ -25,10 +28,24 @@ class LibrarySearch {
             <h4>${library.name}</h4>
               <p>${library.locationName}<br>${library.address}<br>${library.city}, ${library.region} ${library.countryCode}<br>(1 of ${library.branchIds.length} branches)</p>
               </div>
-            <button data-library-website-id="${library.websiteId}" onclick="addLibbyLibrary(this)">Add Lib.</button></div>`;
+            <button data-library-website-id="${library.websiteId}" data-added="false" onclick="${this.btnOnclick}">Add Lib.</button></div>`;
         });
         newHTML = newHTML.join('');
         librariesResultContainer.innerHTML = newHTML;
+    }
+    toggleLibrary(btn) {
+        var libraryWebsiteId = btn.getAttribute("data-library-website-id");
+        var library = this.libraryResults[libraryWebsiteId];
+        var added = btn.getAttribute("data-added");
+        if (added == "true") {
+            this.overdriveInstance.removeLibrary(library);
+            btn.setAttribute("data-added", "false");
+            btn.textContent = "Add Lib.";
+        } else {
+            this.overdriveInstance.addLibrary(library);
+            btn.setAttribute("data-added", "true");
+            btn.textContent = "Remove Lib.";
+        }
     }
 }
 class BookSearch {
