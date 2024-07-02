2b8e0728954a3871b0127d1e5393f65ba01380cc
AdamA-A <adammnashour@gmail.com>
2024-07-02 16:51:00 -0400

    Started implementing the function 'test', which will later morph into fetchAllWithMutedExceptions

--- GAS/Code.js
+++ GAS/Code.js
@@ -17,6 +17,20 @@ const FAVICON_URL = "https://i.imgur.com/N1r2dp3.png";
 const ERRS = {
   0: "Couldn't find last page."
 };
+function test() {
+  var title = "Ruin and Rising";
+  var author = "Leigh Bardugo";
+  var concatTitleAuthor = (title + ' by ' + author).replaceAll(' ', '-').toLocaleLowerCase();
+  var publicUrl = "https://www.epub.pub/book/" + concatTitleAuthor;
+  var fetched = UrlFetchApp.fetch(publicUrl, {
+    muteHttpExceptions: true
+  });
+  // Error is 404
+  var file = DriveApp.createFile("test.txt", JSON.stringify(JSON.stringify({"code": fetched.getResponseCode(), "text": fetched.getContentText()})));
+  Logger.log(file.getUrl());
+  Utilities.sleep(9000);
+  file.setTrashed(true);
+}
 function doGet(e) {
   var template = HtmlService.createTemplateFromFile(CURRENTLY_DEPLOYING.fileName)
     .evaluate()
