6bec554725d7e5197a830713b764b94eff50ff0e
AdamA-A <adammnashour@gmail.com>
2024-07-27 19:13:02 -0400

    Fixed issue between GAS client and server, where fetched URLs didn't transfer to the client side

--- GAS/Client Functions.js
+++ GAS/Client Functions.js
@@ -1,4 +1,11 @@
 // These functions are to be run from the client side
-function client_fetchAllWithMutedExceptions(urlWithParametersObjects) {
-    return JSON.stringify(UrlFetchApp.fetchAll(urlWithParametersObjects));
+function client_fetchAllWithMutedExceptions(urlWithParametersObjects = ["https://www.epub.pub/book/ruin-and-rising-by-leigh-bardugo", "https://www.epub.pub/book/ruin-and-rising-by-leigh-bardugo"]) {
+    /*Logger.log("To fetch:")
+    let fetched = UrlFetchApp.fetchAll(urlWithParametersObjects);
+    Logger.log(fetched)
+    Logger.log("JSON Stringified:")
+    let stringified = JSON.stringify(fetched.map(x => x.getContentText()));
+    Logger.log(stringified);
+    return stringified; */
+    return UrlFetchApp.fetchAll(urlWithParametersObjects).map(x => {return {"code": x.getResponseCode(), "text": x.getContentText()}});
 }
